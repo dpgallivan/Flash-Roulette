9d1728448da95c5ec13d767de24ceb96ab59fba2
Monbedon <monbedon@gmail.com>
2017-08-22 21:18:03 -0700

    add login page

---
+++ public/javascript/login.js
@@ -0,0 +1,15 @@
+$("#createButton").click(function(){
+  $("#creaButton").toggleClass("is-active");  
+});
+
+$("#create-modal-background").click(function(){
+   $("#creaButton").toggleClass("is-active"); 
+})
+
+$("#loginButton").click(function(){
+  $("#logButton").toggleClass("is-active");  
+});
+
+$("#login-modal-background").click(function(){
+   $("#logButton").toggleClass("is-active"); 
+})
