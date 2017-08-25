7ed80b2ccc589ca91ae0cab1bb2b7d290a24fef5
YETC7 <tyler_726@live.com>
2017-08-24 18:47:02 -0700

    Adjust public folder

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -18,56 +18,12 @@ $("#login-modal-background").click(function(){
 
   var currentURL = window.location.origin;
 
-<<<<<<< HEAD
-  $("#createAcctBtn").on("click",function(event) {
-=======
   $("#createUser").on("click",function(event) {
->>>>>>> f4d9890a39f5e554356673e98def138bf90cc9b6
     event.preventDefault();
 
     var nameVal = $("#createUserName").val().trim();
     var passwordVal = $("#createUserPassword").val().trim();
 
-<<<<<<< HEAD
-    console.log(nameVal);
-    console.log(passwordVal);
- 	
- 	//HOLDING NEW USER INFO IN TEMPORARY OBJECT----------------------------------------------------
-    newObject = {
-       username: nameVal,
-       password: passwordVal
-    };
-    console.log(newObject);
-
-    //PUSHING THE NEW OBJECT TO THE DATABASE---------------------------------------------------
-    var mysql = require('mysql');
-
-	var con = mysql.createConnection({
-	  host: "localhost",
-	  user: nameVal,
-	  password: passwordVal,
-	  database: "roulette_db"
-	});
-
-	con.connect(function(err) {
-	  if (err) throw err;
-	  console.log("Connected!");
-	  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-	  con.query(sql, function (err, result) {
-	    if (err) throw err;
-	    console.log("1 record inserted");
-	  });
-	});
-
-     //CLEARS THE INPUT BOXES----------------------------------------------------------------------
-     $("#createUserName").val("");
-     $("#createUserPassword").val("");
-
-
-
-    //ERICS CODE--------------------------------------
-=======
->>>>>>> f4d9890a39f5e554356673e98def138bf90cc9b6
     // Form validation
     if (nameVal === "" || passwordVal === "") {
     	$("#createUserError").html("*All fields are required*");
@@ -98,8 +54,4 @@ $("#login-modal-background").click(function(){
       window.location = currentURL + "/roulette/" + data.userId;
     });
 
-<<<<<<< HEAD
   });
-=======
-  });
->>>>>>> f4d9890a39f5e554356673e98def138bf90cc9b6
