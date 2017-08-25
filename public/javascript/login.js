21a11e4813d2fe2f57e7975333a174b5ffdc307f
edricwong28 <edricwong28@gmail.com>
2017-08-24 17:31:26 -0700

    update create-user and user-login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -13,3 +13,82 @@ $("#loginButton").click(function(){
 $("#login-modal-background").click(function(){
    $("#logButton").toggleClass("is-active"); 
 })
+
+  
+
+  var currentURL = window.location.origin;
+
+  $("#createAcctBtn").on("click",function(event) {
+    event.preventDefault();
+
+    var nameVal = $("#createUserName").val().trim();
+    var passwordVal = $("#createUserPassword").val().trim();
+
+    console.log(nameVal);
+    console.log(passwordVal);
+ 	
+ 	//HOLDING NEW USER INFO IN TEMPORARY OBJECT----------------------------------------------------
+    newObject = {
+       username: nameVal,
+       password: passwordVal
+    };
+    console.log(newObject);
+
+    //PUSHING THE NEW OBJECT TO THE DATABASE---------------------------------------------------
+    var mysql = require('mysql');
+
+	var con = mysql.createConnection({
+	  host: "localhost",
+	  user: nameVal,
+	  password: passwordVal,
+	  database: "roulette_db"
+	});
+
+	con.connect(function(err) {
+	  if (err) throw err;
+	  console.log("Connected!");
+	  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+	  con.query(sql, function (err, result) {
+	    if (err) throw err;
+	    console.log("1 record inserted");
+	  });
+	});
+
+     //CLEARS THE INPUT BOXES----------------------------------------------------------------------
+     $("#createUserName").val("");
+     $("#createUserPassword").val("");
+
+
+
+    //ERICS CODE--------------------------------------
+    // Form validation
+    if (nameVal === "" || passwordVal === "") {
+    	$("#createUserError").html("*All fields are required*");
+        $("#createUserError").show();
+        return;
+    }
+
+    $("#createUserError").hide();
+
+
+
+    // Creates user object
+    var newUser = {
+      "user_name":nameVal,
+      "password":passwordVal,
+      "money": 100 // Default Value for now
+    };
+
+    // Clears fields
+    $("#creaButton").toggleClass("is-active");
+    $("#createUserName").val("");
+    $("#createUserPassword").val("");
+
+    // POSTs new user
+    $.post(currentURL + "/api/signup", newUser, function(data) {
+      // console.log(data);
+      // login(data.userId);
+      window.location = currentURL + "/roulette/" + data.userId;
+    });
+
+  });
